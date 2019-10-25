836d708c27a75d3b558c609f5d6bcd03b42826f5
David Rothfarb <david@dr809.dfci.harvard.edu>
2019-10-25 13:58:35 -0400

    Making good progress: found how to get group names/ids and term names/ids of current post. Next need to query db to match current terms with term group names

--- includes/TagGroupWidget.php
+++ includes/TagGroupWidget.php
@@ -17,7 +17,24 @@ class TagGroupWidget extends WP_Widget {
   }
 
   public function widget($args, $inst) {
-    echo "<h1>This is a test</h1>";
+    global $wpdb;
+    //$results = $wpdb->get_results("SELECT $wpdb->;terms.name, $wpdb->;terms.term_group FROM $wpdb->;terms WHERE $wpdb->;terms.term_group NOT = 0");
+    /* GET GROUP LABELS */ $labels = get_option('term_group_labels', array('result'=> 'nada'));
+    /*GET GROUP POSITIONS */ $positions = get_option('term_group_positions', array('result'=> 'nada'));
+    print_r($results);
+    // GET ALL TERMS $results = get_terms( array('taxonomy' => 'post_tag'));
+    $post = get_the_ID();
+    $tag_arr = get_the_tags($post);
+    //print_r($tag_arr);
+    /*
+    echo "<ul>";
+    foreach($tag_arr as $tag) {
+      echo "<li><strong>Tag name: </strong>$tag->name</li>";
+    }
+    echo "</ul>";
+    */
+      
+
   }
 
   public function form($inst) {
@@ -28,4 +45,3 @@ class TagGroupWidget extends WP_Widget {
 }
 
 ?>
-
