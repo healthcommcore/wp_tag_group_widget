573ebb41b54209ecb1de5e3126d4a023fd9db639
David Rothfarb <david@dr809.dfci.harvard.edu>
2019-10-31 15:25:05 -0400

    Got it working! Need to do some validation, refactoring and maybe modularizing but functionality is there!

--- includes/TagGroupWidget.php
+++ includes/TagGroupWidget.php
@@ -21,11 +21,24 @@ class TagGroupWidget extends WP_Widget {
     //$results = $wpdb->get_results("SELECT $wpdb->;terms.name, $wpdb->;terms.term_group FROM $wpdb->;terms WHERE $wpdb->;terms.term_group NOT = 0");
     /* GET GROUP LABELS */ $labels = get_option('term_group_labels', array('result'=> 'nada'));
     /*GET GROUP POSITIONS */ $positions = get_option('term_group_positions', array('result'=> 'nada'));
-    print_r($results);
+    //print_r($labels);
     // GET ALL TERMS $results = get_terms( array('taxonomy' => 'post_tag'));
     $post = get_the_ID();
-    $tag_arr = get_the_tags($post);
-    //print_r($tag_arr);
+    $tag_arr = $this->makeTagArray( get_the_tags($post) );
+    $tag_sql = $this->getTagGroupSQLStatement($tag_arr);
+    $sql_arr = $wpdb->get_results("SELECT name, term_group FROM $wpdb->terms WHERE $tag_sql");
+    $output_arr = $this->makeTagGroupOutputArray($sql_arr, $labels);
+    $output = "<ul>";
+    foreach($output_arr as $key => $arr) {
+      $output .= "<li>$key</li><ul>";
+        foreach($arr as $tag) {
+          $output .= "<li>$tag</li>";
+        }
+      $output .= "</ul>";
+    }
+    $output .= "</ul>";
+    echo $output;
+
     /*
     echo "<ul>";
     foreach($tag_arr as $tag) {
@@ -42,6 +55,36 @@ class TagGroupWidget extends WP_Widget {
 
   public function update($new_inst, $old_inst) {
   }
+
+  private function makeTagArray($tagObjs) {
+    $tag_arr = [];
+    foreach($tagObjs as $obj) {
+      $tag_arr[]= $obj->term_id;
+    }
+    return $tag_arr;
+  }
+
+  private function getTagGroupSQLStatement($tag_arr) {
+    $sql = "";
+    foreach($tag_arr as $key => $val) {
+      $sql .= "term_id=$val";
+      $sql .= $key < ( count($tag_arr) - 1) ? " or " : ";";
+    }
+    return $sql;
+  }
+
+  private function makeTagGroupOutputArray($sql_arr, $labels) {
+    $output = [];
+    foreach($sql_arr as $obj) {
+      $label = $labels[$obj->term_group];
+      if (!isset($output[$label])) {
+        $output[$label] = [];
+      }
+      $output[$label][] = $obj->name;
+    }
+    return $output;
+  }
+
 }
 
 ?>
